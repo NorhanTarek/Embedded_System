7c6a0c22db1218407c3b21a6b0a4b67ad4937da1
Norhan Tarek <norhantarek@outlook.com>
2020-08-26 23:31:04 +0200

    Adding code to find the largest number among three numbers

---
+++ C_Programing/assignment/h2/EX_3/main.c
@@ -0,0 +1,28 @@
+/*
+ * main.c
+ *
+ *  Created on: Aug 26, 2020
+ *      Author: Norhan
+ */
+
+#include "stdio.h"
+
+void main()
+
+{
+	float a, b, c;
+	printf("Enter Three Numbers: ");
+	fflush (stdin);
+	fflush (stdout);
+	scanf ("%f %f %f", &a, &b, &c);
+
+	/*
+	 compare a with b then store the largest number in a
+     then compare the result with c
+	 */
+	a = (a>=b) ? a : b;
+	a = (a>=c) ? a : c;
+	printf("Largest number = %f", a);
+
+
+}
