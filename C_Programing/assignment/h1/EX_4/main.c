275d67f22d958e9df3acbfb51bcac8dfcbce920e
Norhan Tarek <norhantarek@outlook.com>
2020-08-26 19:05:37 +0200

    Adding code to multiply two floating point numbers

---
+++ C_Programing/assignment/h1/EX_4/main.c
@@ -0,0 +1,29 @@
+/*
+ * main.c
+ *
+ *  Created on: Aug 26, 2020
+ *      Author: Norhan
+ */
+
+/*
+ * main.c
+ *
+ *  Created on: Aug 26, 2020
+ *      Author: Norhan
+ */
+
+#include "stdio.h"
+
+void main ()
+{
+	float x, y;
+	printf ("Enter two numbers: ");
+	fflush (stdin);
+	fflush (stdout);
+	scanf ("%f %f", &x, &y);
+	printf("Product: %f", x*y);
+
+
+
+}
+
