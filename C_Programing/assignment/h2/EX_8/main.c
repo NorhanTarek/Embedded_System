3e491f37dae89cf4a6c4562c47ed421963a6c2ce
Norhan Tarek <norhantarek@outlook.com>
2020-08-27 02:06:27 +0200

    firt release

---
+++ C_Programing/assignment/h2/EX_8/main.c
@@ -0,0 +1,53 @@
+/*
+ * main.c
+ *
+ *  Created on: Aug 27, 2020
+ *      Author: Norhan
+ */
+
+#include "stdio.h"
+void main ()
+{
+	int x, y;
+	char c;
+	printf ("Enter operater either + or - or * or divide: ");
+	fflush(stdin);
+	fflush(stdout);
+	scanf("%c", &c);
+
+	printf ("Enter two operands: ");
+	fflush(stdin);
+	fflush(stdout);
+	scanf("%d %d", &x, &y);
+
+	switch (c)
+	{
+	case '+' :
+
+		printf ("%d + %d = %d", x, y, x+y);
+
+		break;
+	case '-' :
+
+		printf ("%d - %d = %d", x, y, x-y);
+
+		break;
+
+	case '*' :
+
+		printf ("%d * %d = %d", x, y, x*y);
+
+		break;
+
+	case '/' :
+
+		printf ("%d / %d = %d", x, y, x/y);
+
+		break;
+	}
+
+}
+
+
+
+
