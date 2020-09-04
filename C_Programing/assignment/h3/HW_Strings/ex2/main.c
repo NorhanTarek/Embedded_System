32dac31e77baba4652c2b8e6b71f43df49786efd
Norhan Tarek <norhantarek@outlook.com>
2020-09-04 18:30:59 +0200

    Adding new code

---
+++ C_Programing/assignment/h3/HW_Strings/ex2/main.c
@@ -0,0 +1,28 @@
+/*
+ * main.c
+ *
+ *  Created on: Sep 4, 2020
+ *      Author: norha
+ */
+
+
+#include "stdio.h"
+
+void main ()
+{
+	char str [100];
+	//char c;
+	int i, n = 0;
+	printf("Enter a string: ");
+	fflush(stdin);
+	fflush(stdout);
+	gets(str);
+
+
+	for (i=0; str[i]!= 0; i++)
+	{
+		n++;
+	}
+	printf("\r\nlength of string  %d", n );
+
+}
